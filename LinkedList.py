38d592f4f682dd01a5bc3c0aa24c788984152110
enoc pineda <enocpinedaa@gmail.com>
2021-09-26 23:00:13 -0500

    Estructura de datos en python

---
+++ LinkedList.py
@@ -0,0 +1,44 @@
+class Node:
+
+    def __init__ (self, data):
+        self.data = data 
+        self.next = None 
+
+class LinkedList:
+
+    def __init__(self):
+        self.head = None
+
+    def push(self, new_data):
+        new_node = Node(new_data)
+        new_node.next = self.head
+        self.head = new_node
+
+    def insertAfter(self, prev_node, new_data):
+        if prev_node is None:
+            print("The given previous node must in LinkedLisr")
+            return
+        
+        new_node = Node(new_data)
+        new_node.next = prev_node.next
+        prev_node.next = new_node
+
+    def imprimir(self):
+        temp = self.head
+        while (temp):
+            print(temp.data)
+            temp = temp.next
+
+if __name__ =='__main__':
+
+    llist = LinkedList()
+
+    llist.head = Node(1)
+    second = Node(2)
+    third = Node(3)
+
+
+    llist.head.next = second
+    second.next = third
+
+    llist.imprimir()
