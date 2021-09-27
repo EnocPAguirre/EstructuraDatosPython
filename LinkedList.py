be12126bee8eaf902a738566876e6b4e07b577a0
enoc pineda <enocpinedaa@gmail.com>
2021-09-26 23:18:54 -0500

    Linkedit append method

--- LinkedList.py
+++ LinkedList.py
@@ -13,6 +13,19 @@ class LinkedList:
         new_node = Node(new_data)
         new_node.next = self.head
         self.head = new_node
+    
+    def append(self, new_data):
+        new_node = Node(new_data)
+
+        if self.head is None:
+            self.head = new_node
+            return
+
+        last = self.head
+        while (last.next):
+            last = last.next
+
+        last.next = new_node
 
     def insertAfter(self, prev_node, new_data):
         if prev_node is None:
